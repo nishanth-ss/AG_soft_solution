6bc810b068db8940adb4dda0e73847e71abe8489
ajayanandgummi-glitch <ajayanand.gummi@gmail.com>
2025-12-25 15:26:06 +0530

    updatedcopyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="py-5 mt-5 bg-gray-300">
             <div>
-                <h6 className="text-center text-gray-600 px-2">© Copyright © 2020 All rights reserved | Powered By AG Soft Solutions</h6>
+                <h6 className="text-center text-gray-600 px-2">© Copyright © 2026 All rights reserved | Powered By AG Soft Solutions</h6>
             </div>
         </footer>
     );
